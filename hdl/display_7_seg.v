d0f8a6784a836bacbf1dd73298d60f64cfde08a9
Tyler(Zihe) Deng <tyler.dzh@gmail.com>
2020-08-09 14:20:39 -0700

    added 7_seg_display module
    Has been simulated

---
+++ hdl/display_7_seg.v
@@ -0,0 +1,49 @@
+// This module can be clocked or non-clocked, I'm doing it without clock:
+module display_7_seg (
+	input [3:0] i_binary,
+	output o_Seg_a,
+	output o_Seg_b,
+	output o_Seg_c,
+	output o_Seg_d,
+	output o_Seg_e,
+	output o_Seg_f,
+	output o_Seg_g);
+	
+	reg [6:0] r_Hex_Encoding = 7'h00;
+
+	always @ (i_binary)
+	begin
+		case (i_binary)
+		4'b0000: // 0
+		r_Hex_Encoding = 7'h7E;
+		4'b0001:// 1
+		r_Hex_Encoding = 7'h30;
+		4'b0010:// 2
+		r_Hex_Encoding = 7'h6D;
+		4'b0011:// 3
+		r_Hex_Encoding = 7'h79;
+		4'b0100:// 4
+		r_Hex_Encoding = 7'h33;
+		4'b0101:// 5
+		r_Hex_Encoding = 7'h5B;
+		4'b0110:// 6
+		r_Hex_Encoding = 7'h5F;
+		4'b0111:// 7
+		r_Hex_Encoding = 7'h70;
+		4'b1000:// 8
+		r_Hex_Encoding = 7'h7F;
+		4'b1001:// 9
+		r_Hex_Encoding = 7'h7B;
+		default:
+		r_Hex_Encoding = 7'h7E;
+		endcase
+	end
+
+	assign o_Seg_a = r_Hex_Encoding[6];
+	assign o_Seg_b = r_Hex_Encoding[5];
+	assign o_Seg_c = r_Hex_Encoding[4];
+	assign o_Seg_d = r_Hex_Encoding[3];
+	assign o_Seg_e = r_Hex_Encoding[2];
+	assign o_Seg_f = r_Hex_Encoding[1];
+	assign o_Seg_g = r_Hex_Encoding[0];
+endmodule
---
+++ simulation/display_7_seg_tb.v
@@ -0,0 +1,53 @@
+`include "display_7_seg.v"
+`timescale 1us/1ns
+
+module display_7_seg_tb ();
+
+reg [3:0] i_binary = 4'b0000;
+wire o_Seg_a;
+wire o_Seg_b;
+wire o_Seg_c;
+wire o_Seg_d;
+wire o_Seg_e;
+wire o_Seg_f;
+wire o_Seg_g;
+
+
+	display_7_seg UUT (
+		.i_binary(i_binary),
+		.o_Seg_a (o_Seg_a),
+		.o_Seg_b (o_Seg_b),
+		.o_Seg_c (o_Seg_c),
+		.o_Seg_d (o_Seg_d),
+		.o_Seg_e (o_Seg_e),
+		.o_Seg_f (o_Seg_f),
+		.o_Seg_g (o_Seg_g));
+
+	initial begin
+		#10;
+		i_binary = 4'b0000;
+		#10;
+		i_binary = 4'b0001;
+		#10;
+		i_binary = 4'b0010;
+		#10;
+		i_binary = 4'b0011;
+		#10;
+		i_binary = 4'b0100;
+		#10;
+		i_binary = 4'b0101;
+		#10;
+		i_binary = 4'b0110;
+		#10;
+		i_binary = 4'b0111;
+		#10;
+		i_binary = 4'b1000;
+		#10;
+		i_binary = 4'b1001;
+		#10;
+		i_binary = 4'b1110;
+		#10;
+
+		$finish;
+	end
+endmodule
